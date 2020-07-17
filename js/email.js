31112f28eacafab020e09445d5bb3191072d12dd
smerritt3nr2 <smerritt3nr2@yahoo.com>
2020-07-17 17:46:15 +0100

    Combines Email & Image Variables

--- js/email.js
+++ js/email.js
@@ -7,21 +7,31 @@ window.onload = getImg; /* When window Page is loaded, Run Function */
 function getImg() { 
     var img = document.createElement('img');    /* Create new "Image Element" when needed */
     img.src = 'https://source.unsplash.com/random'; /* Stores Random Image in Variable */
+    localStorage.setItem("img", img);
     document.getElementById('location').appendChild(img); /* Places the New Element inside the id called "Location" */
+    document.getElementsByTagName("img")[0].setAttribute("id", "details"); /* Gives the new "Image Element" a ID Function */
 }
 
 /*=============================================================================*/
 
 /*==============================================================================
- * When Buttom is Pressed Store Image in a Variable
+ * When Buttom is Pressed Store Image in a Variable with Email Variable
  ==============================================================================*/
 
  function test() {
 
     var input = document.getElementById('eInput').value;
-    alert(input);
-
-    console.log(input);
+    var image = document.getElementById('details').value;
+    var full = [input, image];
+    console.log(full);
  }
+ 
+ /*=============================================================================*/
+
+ /*==============================================================================
+ * Checks that the Email Values are Valid before being stored
+ ==============================================================================*/
+
+ 
  
  /*=============================================================================*/
